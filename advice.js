b8818c9adf24e46927c1ee9d3b7fdb8b9281f40f
Adeoti Shuaib <Adeotishuaib2001@gmail.com>
2022-04-21 21:10:57 +0100

    Added animation to the toggle

--- advice.js
+++ advice.js
@@ -2,27 +2,7 @@ const dice = document.getElementById("dice"),
   adviceId = document.getElementById("advice-id"),
   adviceText = document.getElementById("advice-text"),
   toggleBtn = document.querySelectorAll(".toggle-btn");
-/*  
 
-View the optimal layout for the app depending on their device's screen size.
-
-Make API calls to the Advice Slip’s API to get the advice
-
-something extra:-
-
-- generate new advice every 10sec, apart from the dice icon which also will generate new advice when clicked.
-
-- create a light mode for the app, the current app design is in dark mode, create a toggle button at the top right of the page which will toggle the mode. Take note of transition effects, might award marks for it :}.
-
-Download the project using the link above and go through the README.md file. This will provide further details about the project and help you get set up.
-
-May the odds be ever in your favor and may the force be with you, remember that Google is your best friend. */
-dice.addEventListener("click", displayAdvice);
-toggleBtn.forEach((btn) => {
-  btn.addEventListener("click", toggleMode);
-});
-setInterval(displayAdvice, 10000);
-function toggleMode() {}
 function displayAdvice() {
   async function getAdvice() {
     try {
@@ -51,3 +31,10 @@ function toggleMode(e) {
   }
   document.documentElement.setAttribute("color-mode", "light");
 }
+
+dice.addEventListener("click", displayAdvice);
+toggleBtn.forEach((btn) => {
+  btn.addEventListener("click", toggleMode);
+});
+
+setInterval(displayAdvice, 10000);
